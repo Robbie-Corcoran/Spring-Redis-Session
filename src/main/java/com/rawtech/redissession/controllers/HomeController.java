b5f7d2b1fd7060dd27aab1848026c0eafe2f78ea
Robbie Corcoran <codewithrobbie@gmail.com>
2023-11-13 12:39:11 +0100

    Added HOME_VIEW_COUNT to Controller and method to increment it.

--- src/main/java/com/rawtech/redissession/controllers/HomeController.java
+++ src/main/java/com/rawtech/redissession/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.rawtech.redissession.controllers;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,18 @@ import java.security.Principal;
 @RestController
 public class HomeController {
 
+    private final String HOME_VIEW_COUNT = "HOME_VIEW_COUNT";
+
     @GetMapping("/")
-    public String home(Principal principal) {
+    public String home(Principal principal, HttpSession session) {
+        incrementCount(session, HOME_VIEW_COUNT);
         return "Hello, " + principal.getName();
     }
+
+    private void incrementCount(HttpSession session, String attribute) {
+        var homeViewCount = session.getAttribute(attribute) == null ? 0 : (Integer) session.getAttribute(attribute);
+        session.setAttribute(attribute, homeViewCount + 1);
+    }
+
 
 }
